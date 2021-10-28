e8408c14a4d95e0c753ae30eb60bf2e50f249686
Eduardo <eduardo.acosta@imagemaker.com>
2021-10-27 19:47:23 -0500

    update

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { json } = require('body-parser');
-const port = 3000;
-    
+const port = 5001;
+
 app.get('/api/v/:id', function (req, res) {
 
   const fileId = req.params.id;
@@ -27,8 +27,14 @@ app.get('/api/v/:id', function (req, res) {
 
 })
 
+app.use(cors({
+    origin: ['https://wizcontent.vercel.app']
+}));
 app.use(bodyParser.json());
-app.listen(port, () => {
+
+var server = app.listen(port);
+server.keepAliveTimeout = 30000;
+/*app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+})*/
 module.exports = app;
